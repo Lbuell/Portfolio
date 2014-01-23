c48be049e6e8fe23f5ddc862f95a081335c411e9
lbuell <liamcbuell@gmail.com>
2014-01-22 18:57:37 -0800

    Added Devise

---
+++ test/features/auth/sign_in_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+feature "user sign in" do
+  scenario "user signs in" do
+    # Given an existing user
+    visit root_path
+    click_on "Sign In"
+
+    # When the user creates a new session
+    fill_in "Email", with: users(:liam).email
+    fill_in "Password", with: "password"
+    click_on "Sign in"
+
+    # Then the user should be signed in
+    page.must_have_content "Signed in successfully"
+    page.wont_have_content "Sign In"
+  end
+end
---
+++ test/features/auth/sign_out_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+feature "
+  As a user,
+  I want to sign out when I'm done
+  so that my session is closed." do
+  scenario "sign out current user" do
+    # Given a current user
+    visit user_session_path
+    fill_in "Email", with: users(:dude).email
+    fill_in "Password", with: "password"
+    click_on "Sign in"
+
+    page.must_have_content "Signed in successfully."
+    save_and_open_page
+    page.must_have_content "Sign out"
+    # When the sign out link is clicked
+    click_on "Sign out"
+
+    # Then the session should be destroyed
+    page.must_have_content "Signed out successfully"
+    page.wont_have_content "Sign Out"
+  end
+end
