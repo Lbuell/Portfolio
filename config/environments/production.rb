d79032f984875d8ac524f9cc2a5444dde4be46bd
lbuell <liamcbuell@gmail.com>
2014-02-24 15:13:30 -0800

    smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,14 +15,17 @@ Portfolio::Application.configure do
   config.action_controller.perform_caching = true
 
 
+  config.action_mailer.default_url_options = { :host => 'www.liambuell.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "liambuell.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
 }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
